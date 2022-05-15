64f9d83955de446df85ed32ffab5b7fcb5081a22
seolminsu90 <seolminsu90@naver.com>
2022-05-15 10:50:59 +0900

    init

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Release Date"), DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        // Date, int 등의 형식은 기본이 Required. 불필요 시 DateTime? 과 같이 Nullable로 지정하면 된다.
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1, 100), DataType(DataType.Currency)]// View engine에 대한 힌트 제공 Date, Time, PhoneNumber, Currency, EmailAddress 등..
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
+        public string Rating { get; set; } = string.Empty;
+    }
+
+    /*
+     * 
+     *  Add-Migration {마이그레이션네임}
+        Update-Database
+    */
+}
---
+++ RazorPagesMovie/Pages/Movies/Edit.cshtml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class EditModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public EditModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie =  await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            Movie = movie;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Attach 이전 수정 사항은 저장되지 않는다? -? ? 라는데
+            _context.Attach(Movie).State = EntityState.Modified; // Entity 상태작업 - 수정
+
+            // 모델의 현재값을 읽고 새 값으로 전환하는 것
+            _context.Entry(Movie).Property(m => m.Rating).CurrentValue = "SSR";
+            Console.WriteLine("Rating {0}", Movie.Rating); // => SSR
+            Console.WriteLine(_context.Entry(Movie).Property(m => m.Rating).OriginalValue); // 변경 전 값
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(Movie.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MovieExists(int id)
+        {
+          return (_context.Movie?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
